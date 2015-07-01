f4735188f14e6139bd4e4a93c6f7d66d11cc99a4
Chad-Barlow <triforce.dragon@gmail.com>
2015-07-01 13:37:21 -0600

    First Commit and start of plugin

---
+++ LinkTools/src/main/java/com/gmail/triforcedragon/LinkTools/LinkTools.java
@@ -0,0 +1,17 @@
+package com.gmail.triforcedragon.LinkTools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LinkTools extends JavaPlugin {
+
+	@Override
+    public void onEnable() {
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+ 
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+
+}
